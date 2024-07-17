7f4db70853a1ff1b1f82055a57cb51986d93b90d
danniel0908 <159111543+danniel0908@users.noreply.github.com>
2024-07-17 22:32:20 +0800

    Delete try directory

--- try/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-global $mysqli;
-$mysqli = new mysqli("localhost", "root", "Leviathan0908", "user");
-
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-?>
